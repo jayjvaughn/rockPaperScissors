776931f8d8eb3a108578d0fd0fc387da1c009008
jayjvaughn <waveyjxyshop@gmail.com>
2024-03-06 23:23:00 -0500

    Solution to why playGame funtion wasnt working
    my code was fine through all 50 changed i made, the issue was that I wasnt running the fucntion properly to test everything. I was running playRound(playerSelection, computerSelection); in the console rather than running console.log(playRound(playerSelection,getComputerChoice())).. I wasted days thinking it was an error in my if else when really the error was in my understanding of how to run all my code together.

--- script.js
+++ script.js
@@ -2,33 +2,32 @@
 
 
 const choices = ["rock", "paper", "scissors"]
-const computerSelection = getComputerChoice();
-const rock = "rock"
-const paper = "paper"
-const scissors = "scissors"
+const computerSelection = choices[Math.floor(Math.random() * choices.length )];
+let playerSelection = prompt("");
 
-// let rock = ("rock" < "paper" || "rock" > "scissors" || "rock" == "rock")
- //let paper = ("paper" > "rock" || "paper" < "scissors" || "paper" == "paper")
-// let scissors = ("scissors" < "rock" || "scissors" > "paper" || "scissors" == "scissors")
 
 
  function getComputerChoice(){
      const computerSelection = choices[Math.floor(Math.random() * choices.length )];
      return computerSelection;
 
- console.log(computerSelection);
+//  console.log(computerSelection);
  }
 
+ function playRound(playerSelection, computerSelection){
 
- function playRound(playerSelection, computerSelection) {
-   if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock"){
-    return "you win!";
+    if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock"){
+        return "you win!";
    }
         else if (playerSelection === computerSelection){
-            return "it's a tie!"
-        }
+        return "it's a tie!";
+    }
+   
+
         else {
             return "you lose...sorry";
         }
   }
+ // major change
+  console.log(playRound(playerSelection,getComputerChoice()))
   
